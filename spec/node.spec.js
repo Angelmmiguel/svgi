676b0aebd8c042856850a3ed3725258232609a2b
Angel M <angel@laux.es>
2017-06-12 00:29:16 +0200

    Add dummy tests

---
+++ spec/node.spec.js
@@ -0,0 +1,6 @@
+describe('dummy', () => {
+  it('should work', () => {
+    // TODO: Create tests
+    expect(1).toBe(1);
+  });
+});
--- spec/svg.spec.js
+++ spec/svg.spec.js
@@ -1,1 +1,6 @@
-const SVG = require('../lib/svg');
+describe('dummy', () => {
+  it('should work', () => {
+    // TODO: Create tests
+    expect(1).toBe(1);
+  });
+});
