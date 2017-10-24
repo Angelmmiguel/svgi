c1be2d0c7b031704f7ff21f7ca0e92fc4d35692f
Joshua Fuller <joshuaandrewfuller@gmail.com>
2017-10-24 08:57:16 -0600

    Add stdin functionality (#22)
    * Initial stdin commit
    
    * Fixes code review issues
    
    Addresses comments from code review of PR #22
    
    * Add support for edge case with no stdin or filename
    
    * Update help information

--- bin/index.js
+++ bin/index.js
@@ -11,44 +11,79 @@ const jsonFormatter = require('../lib/formatters/json');
 const yamlFormatter = require('../lib/formatters/yaml');
 const SVG = require('../lib/svg');
 
-// Declare the app
-const app = (filePath, options) => {
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      switch (err.code) {
-        case 'ENOENT':
-          console.error(
-            `${colors.red('[Error]')} There file ${filePath} doesn't exist`);
-          break;
-        default:
-          console.error(
-            `${colors.red('[Error]')} There was an error opening the ` +
-            `file: ${err.message}`);
+const parseStdin = () => {
+  return new Promise((resolve, reject) => {
+    let data = '';
+    
+    process.stdin.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    process.stdin.on('error', (error) => {
+      reject(`${colors.red('[Error]')} There was an error parsing the STDIN data: ${error}`);
+    });
+
+    process.stdin.on('end', function() {
+      resolve(data);
+    });
+  });
+};
+
+const parseFile = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        switch(err.code) {
+          case 'ENOENT':
+            reject(`${colors.red('[Error]')} There file ${filePath} doesn't exist`);
+            break;
+          default:
+            reject(`${colors.red('[Error]')} There was an error opening the ` +
+              `file: ${err.message}`);
+        }
+      } else {
+        resolve(data);
       }
-    } else {
-      // Load the SVG
-      try {
+    });
+  });
+};
+
+// Declare the app
+const app = (filePath) => {
+  let dataPromise;
+
+  if (!process.stdin.isTTY) {
+    dataPromise = parseStdin()
+  } else if (filePath) {
+    dataPromise = parseFile(filePath)
+  } else {
+    commander.help();
+  }
+
+  dataPromise
+    .then((data) => {
         let svg = new SVG(data, filePath);
         let output;
 
-        switch (options.output) {
+        if(filePath && !process.stdin.isTTY) {
+          commander.dualInputs = true;
+        };
+
+        switch (commander.output) {
           case 'json':
-            output = jsonFormatter(svg, options);
+            output = jsonFormatter(svg, commander);
             break;
           case 'yaml':
-            output = yamlFormatter(svg, options);
+            output = yamlFormatter(svg, commander);
             break;
           default:
-            output = humanFormatter(svg, options);
+            output = humanFormatter(svg, commander);
         }
 
         // Display the output
         console.log(output);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  });
+    })
+    .catch(console.error);
 };
 
 // Parse options and run the CLI
@@ -67,6 +102,12 @@ commander
                      ' Only available for human formatter')
   .option('--legend', 'Show the tree color legend. Only available for human ' +
                       'formatter')
-  .arguments('<file>')
+  .arguments('[file|stdin]')
   .action(app)
   .parse(process.argv);
+
+// If the argument is not passed, commander does not call app,
+// so it must be called manually here
+if (commander.args.length === 0) {
+  app();
+}
